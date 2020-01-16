d1b2a7f7e84ed6bfd191bc2a6b4b7649796f67f7
Silvstedt <silvstedtz@outlook.com>
2020-01-16 13:50:46 -0400

    LoginFirebase

--- VYRMobile/VYRMobile/ViewModels/IdentityViewModel.cs
+++ VYRMobile/VYRMobile/ViewModels/IdentityViewModel.cs
@@ -1,7 +1,4 @@
 ﻿using MvvmHelpers;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using VYRMobile.Models;
 using VYRMobile.Services;
--- VYRMobile/VYRMobile/Views/Loading.xaml.cs
+++ VYRMobile/VYRMobile/Views/Loading.xaml.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Plugin.CloudFirestore;
+using Plugin.CloudFirestore.Extensions;
+using Plugin.DeviceInfo;
+using System;
 using System.Threading.Tasks;
-
+using VYRMobile.Services;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,19 +12,77 @@ namespace VYRMobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Loading : ContentPage
     {
+
+        bool isWaiting;
+        string _appId;
         public Loading()
         {
+            _appId = CrossDeviceInfo.Current.Id;
             InitializeComponent();
+            var document = CrossCloudFirestore.Current.Instance
+                 .GetCollection("usersApp")
+            .GetDocument(_appId)
+            .AsObservable().Subscribe(document =>
+            {
+                var test = document.Data["Status"].ToString();
+
+                if(test == "ACCEPTED")
+                {
+                    isWaiting = false;
+                    Application.Current.MainPage = new NavigationPage(new MenuPage());
+
+                }else if(test == "CANCELED"){
+                    isWaiting = false;
+                    App.IsUserLoggedIn = false;
+                    Application.Current.MainPage = new NavigationPage(new Login());
+
+                }
+                else
+                {
+                    isWaiting = true;
+                }
+            });
+
+            isWaiting = true;
         }
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            charge();
+           
+            Charge();
         }
-        private async void charge()
+
+        protected override void OnDisappearing()
         {
-            await Task.Delay(500);
-            Application.Current.MainPage = new NavigationPage(new MenuPage());
+            base.OnDisappearing();
+
+            
+        }
+
+        private async void Charge()
+        {
+
+
+
+         
+
+            while (isWaiting)
+            {
+                //var result = CheckStatus().ToString();
+
+                //if(result == "ACCEPTED")
+                //{
+                //    isWaiting = false;
+                //}
+                await Task.Delay(100);
+            }
+          
+            //Application.Current.MainPage = new NavigationPage(new MenuPage());
+        }
+
+        public class StatusU
+        {
+            public string Status { get; set; }
         }
     }
 }
