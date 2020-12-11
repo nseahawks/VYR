7f2084ff9c130435176475ac0247720328fa42dd
Ashby Hernandez <asashby@hotmail.com>
2020-12-11 19:28:54 -0400

    feat/ inspection page stashed

---
+++ VYRMobile/VYRMobile/Models/InspectionItem.cs
@@ -0,0 +1,43 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VYRMobile.Models
+{
+    public class InspectionItem : ObservableObject
+    {
+        string id;
+        public string Id
+        {
+            get => id;
+            set => SetProperty(ref id, value);
+        }
+
+        string name;
+        public string Name
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+
+        bool isDone;
+        public bool IsDone
+        {
+            get => isDone;
+            set => SetProperty(ref isDone, value);
+        }
+        bool hasCommentary;
+        public bool HasCommentary
+        {
+            get => hasCommentary;
+            set => SetProperty(ref hasCommentary, value);
+        }
+        string observations;
+        public string Observations
+        {
+            get => observations;
+            set => SetProperty(ref observations, value);
+        }
+    }
+}
---
+++ VYRMobile/VYRMobile/Views/Cells/InspectionItemViewCell.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VYRMobile.Views.Cells
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InspectionItemViewCell : ViewCell
+    {
+        public InspectionItemViewCell()
+        {
+            InitializeComponent();
+        }
+        protected override void OnChildAdded(Element child)
+        {
+            base.OnChildAdded(child);
+
+            uint duration = 750;
+
+            var animation = new Animation();
+
+            animation.WithConcurrent((f) => InspectionItemTemplate.Opacity = f, 0, 1, Easing.CubicOut);
+
+            animation.WithConcurrent(
+              (f) => InspectionItemTemplate.TranslationY = f,
+              InspectionItemTemplate.TranslationY + 0, 50,
+              Easing.CubicOut, 0, 1);
+
+            InspectionItemTemplate.Animate("FadeIn", animation, 16, Convert.ToUInt32(duration));
+        }
+    }
+}
